3b96915088d1bc47209288ab896669fb6e7251c7
idriswadaji@gmail.com <idriswadaji@gmail.com>
2024-03-20 02:17:05 +0400

    Adding the lessons

---
+++ lessons.js
@@ -0,0 +1,83 @@
+const LESSONS = [
+    {
+      id: 1,
+      subject: 'Programming',
+      location: '19 G-0-2 Computer Lab,DKP',
+      price: 50,
+      spaces: 5,
+      icon: 'fas fa-superscript'
+    },
+    {
+      id: 2,
+      subject: 'Chemistry',
+      location: '19 G-1-4,DKP',
+      price: 30,
+      spaces: 3,
+      icon: 'fas fa-flask'
+    },
+    {
+      id: 3,
+      subject: 'History',
+      location: '19 G-3-7,DKP',
+      price: 25,
+      spaces: 8,
+      icon: 'fas fa-history'
+    },
+    {
+      id: 4,
+      subject: 'Literature',
+      location: '20 G-0-7 Library,DKP',
+      price: 20,
+      spaces: 6,
+      icon: 'fas fa-book-open'
+    },
+    {
+      id: 5,
+      subject: 'Anatomy',
+      location: '19 G-1-2,DKP',
+      price: 35,
+      spaces: 4,
+      icon: 'fas fa-heartbeat'
+    },
+    {
+      id: 6,
+      subject: 'Physics',
+      location: '17 G-0-1,DKP',
+      price: 35,
+      spaces: 2,
+      icon: 'fas fa-atom'
+    },
+    {
+      id: 7,
+      subject: 'Algebra',
+      location: '16 G-3-7,DKP',
+      price: 45,
+      spaces: 5,
+      icon: 'fas fa-code'
+    },
+    {
+      id: 8,
+      subject: 'Painting',
+      location: '15 G-2-4 Studio,DKP',
+      price: 30,
+      spaces: 7,
+      icon: 'fas fa-palette'
+    },
+    {
+      id: 9,
+      subject: 'Guitar',
+      location: '18 G17,DKP',
+      price: 15,
+      spaces: 4,
+      icon: 'fas fa-guitar'
+    },
+    {
+      id: 10,
+      subject: 'Yoga',
+      location: '19 G07,DKP',
+      price: 20,
+      spaces: 10,
+      icon: 'fas fa-peace'
+    }
+  ];
+  
